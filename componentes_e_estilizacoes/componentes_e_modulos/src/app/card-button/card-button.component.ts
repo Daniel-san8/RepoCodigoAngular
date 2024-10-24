d63f0ab79f778bae88a0d2e300a105ec924a6d3a
Daniel San <danielgdt88@hotmail.com>
2024-10-24 14:52:29 -0300

    feat: propriedade template

--- componentes_e_estilizacoes/componentes_e_modulos/src/app/card-button/card-button.component.ts
+++ componentes_e_estilizacoes/componentes_e_modulos/src/app/card-button/card-button.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
+  // é possível usar template: `` para html curto
   templateUrl: './card-button.component.html',
-  styleUrl: './card-button.component.scss'
+  styleUrl: './card-button.component.scss',
 })
-export class CardButtonComponent {
-
-}
+export class CardButtonComponent {}
