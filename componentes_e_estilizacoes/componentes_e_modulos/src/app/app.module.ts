55646e7c4de05a3a3d2b29fb6720a291d51af38c
Daniel San <danielgdt88@hotmail.com>
2024-10-24 10:24:50 -0300

    feat: crio dois componentes filhos para cards

--- componentes_e_estilizacoes/componentes_e_modulos/src/app/app.module.ts
+++ componentes_e_estilizacoes/componentes_e_modulos/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, CardRoxoComponent],
+  declarations: [AppComponent, CardComponent, CardRoxoComponent, CardButtonComponent, CardRoxoButtonComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
