0b41cd5dfe5653e2d4a94ef904043c039ee4ad89
Daniel San <danielgdt88@hotmail.com>
2024-10-25 18:04:36 -0300

    feat: começando módulo de bind diretivas templates e decorators

---
+++ bind_diretivas_templates_decorators/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'bind_diretivas_templates_decorators';
+}
