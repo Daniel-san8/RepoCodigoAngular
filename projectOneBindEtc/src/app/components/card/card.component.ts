a28444cb02561be724b94cebc768ec5ba205f1d1
Daniel San <danielgdt88@hotmail.com>
2024-10-31 21:01:09 -0300

    feat: crio logica para componente card e button no card

--- projectOneBindEtc/src/app/components/card/card.component.ts
+++ projectOneBindEtc/src/app/components/card/card.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
+  @Input({ alias: 'planType', required: true }) planType = '';
+  @Input({ alias: 'planPrice', required: true }) planPrice = 0;
 
+  @Input() cardStyle: 'orange' | 'purple' = 'orange';
 }
