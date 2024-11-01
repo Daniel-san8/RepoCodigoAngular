afb930dc2dbc9abe234daca9bcd416d357f14656
Daniel San <danielgdt88@hotmail.com>
2024-10-31 22:43:29 -0300

    feat: criação de module do components

---
+++ projectOneBindEtc/src/app/components/components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ButtonComponent } from './button/button.component';
+import { CardComponent } from './card/card.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [ButtonComponent, CardComponent],
+  imports: [CommonModule],
+  exports: [CardComponent, ButtonComponent],
+})
+export class ComponentsModule {}
