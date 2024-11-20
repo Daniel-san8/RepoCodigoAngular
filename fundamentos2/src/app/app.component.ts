8fa680bea83e2a8344cb8296126e9435fff39d53
Daniel Santos <danielgdt88@hotmail.com>
2024-11-20 15:42:30 -0300

    feat: crio novo projeto de diretiva

--- fundamentos2/src/app/app.component.ts
+++ fundamentos2/src/app/app.component.ts
@@ -1,28 +1,58 @@
-import {AfterViewInit, Component, ElementRef, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnChanges,
+  OnInit, QueryList,
+  SimpleChanges,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterViewInit {
+  arrButtons: string[] = ['Button 1', 'Button 2', 'Button 3']
 
-  @ViewChild('myInput', {static: true}) myInputEL?: ElementRef<HTMLInputElement>
-  @ViewChild('myInput2') myInput2El?: ElementRef<HTMLInputElement>
+  @ViewChildren('meuButton') buttonsEl?: QueryList<ElementRef<HTMLButtonElement>>;
 
-  clicou(input: HTMLInputElement) {
-    console.log(input)
+
+  ngAfterViewInit() {
+    console.log(this.buttonsEl);
   }
 
-  chamaInputEl() {
-    if (this.myInputEL) this.myInputEL.nativeElement.value = 'FUI CLICADO, MAN'
+  changeColor(event: MouseEvent) {
+    const button = event.target as HTMLButtonElement;
+
+    button.style.backgroundColor = 'black'
   }
+
+  resetColor() {
+    this.buttonsEl?.forEach(button => {
+      const buttonElement = button.nativeElement
+      buttonElement.style.backgroundColor = '#F0F0F0'
+    })
+  }
+
+  // @ViewChild('myInput', {static: true}) myInputEL?: ElementRef<HTMLInputElement>
+  // @ViewChild('myInput2') myInput2El?: ElementRef<HTMLInputElement>
+  //
+  // clicou(input: HTMLInputElement) {
+  //   console.log(input)
+  // }
+  //
+  // chamaInputEl() {
+  //   if (this.myInputEL) this.myInputEL.nativeElement.value = 'FUI CLICADO, MAN'
+  // }
 
   // ngAfterViewInit() {
   //   this.myInput2El?.nativeElement.focus()
   // }
 
-  ngOnInit() {
-    this.myInputEL?.nativeElement.focus()
-  }
+  // ngOnInit() {
+  //   this.myInputEL?.nativeElement.focus()
+  // }
 }
