83c0e8f20e53b1144450d84023192c8e78e89fa3
Daniel Santos <danielgdt88@hotmail.com>
2024-11-28 21:46:09 -0300

    feat: inicio projeto e crio comp card

---
+++ projectThree/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './components/common/card/card.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {}
---
+++ projectThree/src/app/components/common/card/card.component.ts
@@ -0,0 +1,16 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, Input, numberAttribute } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {
+  @Input({ required: true, alias: 'iconCard' }) iconCard = '';
+  @Input({ required: true, alias: 'textCard' }) textCard = '';
+  @Input({ required: true, alias: 'valueCard', transform: numberAttribute })
+  valueCard = 0;
+}
