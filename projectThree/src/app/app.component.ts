49cd6940cae8cade4f53d10d2d76330631c02539
Daniel Santos <danielgdt88@hotmail.com>
2024-12-01 13:34:42 -0300

    feat: finish

--- projectThree/src/app/app.component.ts
+++ projectThree/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './components/common/card/card.component';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CardComponent, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
