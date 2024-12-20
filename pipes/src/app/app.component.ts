868a82c009024690ce5c8a308346040cf52606b1
Daniel San <danielgdt88@hotmail.com>
2024-12-20 20:32:46 -0300

    feat: arquivos de pipe

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'pipes';
+  pessoa = {
+    name: 'Daniel San',
+    age: 22,
+  };
 }
